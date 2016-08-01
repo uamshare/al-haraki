a5b33cfb604ab4df512b0832a65dc8299159d559
uam <uamshare@Gmail.com>
2016-08-01 08:29:30 +0700

    Update Angular UI

--- client/web/js/modules/controllers/CoaController.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.controller('CoaController', function ($scope, $http, $window, toaster, ngDialog, $injector, $httpParamSerializer) {
-    var $validationProvider = $injector.get('$validation');
-    $http.defaults.headers.patch['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
-
-    
-
-});
